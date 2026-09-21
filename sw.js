22ed1709b5e45d941ccdc058491ed1926d47b0de
sharadkumar518-alt <sharadkumar518@gmail.com>
2026-09-21 08:45:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "laksh-v3";
+const CACHE = "laksh-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
